e4222585ede9cac4e1297679daca95049f0b245a
Henry Arneson <2017.harneson@jburroughs.org>
2017-02-04 17:57:06 -0600

    oa parsing

--- controllers/rpcMethods/openAlias.js
+++ controllers/rpcMethods/openAlias.js
@@ -1,5 +1,4 @@
 const dnsLookup = require('../../helpers/dnsLookup');
-const rpcRes = require('../../helpers/rpcResponse');
 const config = require('config');
 
 var server;
@@ -7,10 +6,39 @@ if (config.dns.customServer) {
   server = config.dns.server;
 }
 
+function parseTxtRecord(recString) {
+  // NOTE: this function will only work on the most basic case (no double quotes or weird ; placement)
+  recString = recString.trim();
+  if (recString.substring(0,8) != 'oa1:xmr ') return null;
+  recString = recString.substring(8);
+
+  var record = {};
+
+  var ss = recString.split(';');
+  for (var i = 0; i < ss.length; i++) {
+    var pair = ss[i].trim().split('=');
+    if (pair[0] != '') {
+      if (pair.length != 2) return null;
+      record[pair[0]] = pair[1];
+    }
+  }
+  return record;
+}
+
+
 module.exports = function(body) {
   return dnsLookup(body.params.name, server)
     .then((data) => {
-      return {result: data};
+      result = {};
+      for (var i = 0; i < data.length; i++) {
+        var pr = parseTxtRecord(data[i][0]);
+        if (pr) {
+          result = pr;
+          result.record = data[i][0];
+          break;
+        }
+      }
+      return {result: result};
     })
     .catch((err) => {
       var rpcError = {
